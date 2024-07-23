f73d3e07b2a3c5fb1acef5f50018d3eaa6f5c845
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-07-23 21:00:54 +0900

    [ ✨ | #1 | Feat ] 초기세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/HomePage.jsx";
+import { AboutPage } from "./pages/AboutPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/jamooooongPage/",
+    element: <App />,
+    children: [
+      {
+        path: "/jamooooongPage/",
+        element: <HomePage />,
+      },
+      {
+        path: "/jamooooongPage/about",
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,7 @@
+export function AboutPage() {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+    </div>
+  );
+}
