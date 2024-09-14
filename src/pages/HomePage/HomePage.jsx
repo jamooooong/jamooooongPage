ca2b909d8ad94615598fc40a39e7986a0f22d8b1
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-14 15:36:13 +0900

    ✨ feat: 프로필 제작

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,32 @@
+import Profile_Img from "../../../public/imgs/profile_img.png"
+import LinkBtn from "./_components/linkBtn";
+
 export function HomePage() {
   return (
     <>
-      <div className="h-screen">
-        <h1 className="text-3xl font-bold underline font-Pretendard">Jamooooong</h1>
+      <div className="h-screen"> 
+        <div className="flex flex-row mt-56">
+          <div className="flex flex-col w-60">
+            <img src={Profile_Img} alt="profile_img" className=" mb-6"/>
+            <p className="text-red_01 font-semibold text-base my-1">javaScript</p>
+            <p className=" text-3xl font-bold my-1">jamooooong</p>
+            <div className="flex flex-row my-5">
+              <LinkBtn text="github" link="https://github.com/jamooooong" />
+              <LinkBtn text="instagram" link="https://www.instagram.com/jamong__w.s/" />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <p>안녕하세요 박우석입니다.</p>
+            <p>안녕하세요 박우석입니다.</p>
+            <p>안녕하세요 박우석입니다.</p>
+            <p>안녕하세요 박우석입니다.</p>
+            <p>안녕하세요 박우석입니다.</p>
+            <p>안녕하세요 박우석입니다.</p>
+
+          </div>
+        </div>
+        
+        
       </div>
     </>
   );
---
+++ src/pages/HomePage/_components/linkBtn.jsx
@@ -0,0 +1,15 @@
+import LinkIcon from "../../../../public/imgs/svgs/link_icon.svg";
+
+export function LinkBtn({ text, link }) {
+  return (
+    <>     
+        <a href={link} className="text-gray_02 text-base flex flex-row gap-1 mr-2 ">
+            <img src={LinkIcon} />
+            {text}
+        </a>
+   </>
+   
+    );
+}
+
+export default LinkBtn;
