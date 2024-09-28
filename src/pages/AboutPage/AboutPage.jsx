90358742f8cffe14afa2afef3617c79706403d42
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-29 01:32:04 +0900

    [ ✨ | #13 | Feat ] 모든 페이지 레이아웃 설정

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,14 @@
+import { Profile } from '../../ui/_components/profile';
+
 export function AboutPage() {
   return (
     <div className="sm:w-3/5 mx-auto py-40 text-gray_01 px-3">
-      <h1>AboutPage</h1>z
+      <div className="flex sm:flex-row flex-col">
+        <Profile />
+        <div className="flex flex-col mt-20 sm:mt-0 sm:w-2/3 sm:ml-auto">
+          <h1>AboutPage</h1>
+        </div>
+      </div>
     </div>
   );
 }
