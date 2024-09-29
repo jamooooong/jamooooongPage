7bb657e380d201ceae68a7655ab6a55e46f574ea
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-29 18:01:39 +0900

    [ ✨ | #17 | Feat ] skills 페이지 구현

--- src/pages/SkillsPage/SkillsPage.jsx
+++ src/pages/SkillsPage/SkillsPage.jsx
@@ -6,7 +6,26 @@ export function SkillsPage() {
       <div className="flex sm:flex-row flex-col">
         <Profile />
         <div className="flex flex-col mt-20 sm:mt-0 sm:w-2/3 sm:ml-auto">
-          <h1>Skill Page</h1>
+          <p className=" text-4xl text-red_01 font-bold mb-12">Skills</p>
+          <p className=" text-xl font-bold mb-4">Design</p>
+          <div className="flex sm:flex-row flex-col sm:justify-between text-base text-gray_02">
+            <div className="flex flex-col sm:w-1/2 sm:h-[7.5rem]">
+              <p>Adobe Photoshop</p>
+              <p>Adobe Illustrator</p>
+              <p>Adobe Lightroom</p>
+            </div>
+            <div className="flex flex-col sm:w-1/2 sm:h-[7.5rem] sm:mb-0 mb-12">
+              <p>Adobe Figma</p>
+              <p>Lottie (공부중)</p>
+              <p>Phase (공부중)</p>
+            </div>
+          </div>
+          <p className=" text-xl font-bold mb-4">Front-End</p>
+          <div className=" text-base text-gray_02 mb-12">
+            <p>html, css, js, ts</p>
+            <p>React</p>
+            <p>Next.js ( 공부중 )</p>
+          </div>
         </div>
       </div>
     </div>
