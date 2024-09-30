ed88e6cf8306934ce6ccf5356fdd2833ac64cb16
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-30 18:32:51 +0900

    [ ✨ | #25 | Feat ] 사진 곡률 추가

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -9,7 +9,10 @@ export function AboutPage() {
           <div className="flex flex-col mt-20 sm:mt-0 sm:w-2/3 sm:ml-auto gap-12">
             <p className=" text-4xl text-red_01 font-bold">Hobby</p>
             <div className="flex sm:flex-row flex-col w-full gap-4">
-              <img src="https://github.com/user-attachments/assets/449f96ae-404f-4e93-ab1c-cf686bd89c08" />
+              <img
+                src="https://github.com/user-attachments/assets/449f96ae-404f-4e93-ab1c-cf686bd89c08"
+                className=" rounded-lg"
+              />
               <div className="flex flex-col gap-4">
                 <p className=" text-xl font-bold">사진</p>
                 <p className=" text-base text-gray_02">
@@ -21,12 +24,16 @@ export function AboutPage() {
               </div>
             </div>
             <div className="flex sm:flex-row flex-col w-full gap-4">
-              <img src="https://github.com/user-attachments/assets/ad5ac71a-3f71-45ba-a100-cb198a4dcca3" />
+              <img
+                src="https://github.com/user-attachments/assets/ad5ac71a-3f71-45ba-a100-cb198a4dcca3"
+                className=" rounded-lg"
+              />
               <div className="flex flex-col gap-4">
-                <p className=" text-xl font-bold">기계 만지기?</p>
+                <p className=" text-xl font-bold">기계 만지기</p>
                 <p className=" text-base text-gray_02">
-                  어릴때부터 기계를 만지고 뜯어보는 걸 좋아했어요. 컴퓨터
-                  조립하고, 부품들 만지는거 좋아해요. 가끔은 차도 뜯습니다..
+                  어릴때부터 기계를 만지고 분해하는걸 좋아했어요. 컴퓨터
+                  조립하고, 부품들 만지는거 좋아합니다. IT기기에 관심이
+                  많습니다.
                 </p>
               </div>
             </div>
@@ -41,7 +48,10 @@ export function AboutPage() {
             <div className="flex flex-col mt-20 sm:mt-0 sm:w-2/3 sm:ml-auto gap-12">
               <p className=" text-4xl text-red_01 font-bold">Like</p>
               <div className="flex sm:flex-row flex-col w-full gap-4">
-                <img src="https://github.com/user-attachments/assets/ae9ee2da-0968-4fa3-8231-4633078eae6e" />
+                <img
+                  src="https://github.com/user-attachments/assets/ae9ee2da-0968-4fa3-8231-4633078eae6e"
+                  className=" rounded-lg"
+                />
                 <div className="flex flex-col gap-4">
                   <p className=" text-xl font-bold">회</p>
                   <p className=" text-base text-gray_02">
@@ -51,13 +61,16 @@ export function AboutPage() {
                 </div>
               </div>
               <div className="flex sm:flex-row flex-col w-full gap-4">
-                <img src="https://github.com/user-attachments/assets/4b2c9d0c-1d6c-47bb-b382-6e00d13cce62" />
+                <img
+                  src="https://github.com/user-attachments/assets/4b2c9d0c-1d6c-47bb-b382-6e00d13cce62"
+                  className=" rounded-lg"
+                />
                 <div className="flex flex-col gap-4">
                   <p className=" text-xl font-bold">i luv vietnam</p>
                   <p className=" text-base text-gray_02">
                     베트남에 놀러가는게 너무 좋아요. 24년에만 두번 다녀왔어요.
                     음식도 맛있고, 관광지도 좋고, 경치도 좋고, 물가도 저렴해서
-                    즐기기 너무 좋아요. 더운거만 빼면요...
+                    여유를 즐기기 너무 좋아요. 더운거만 빼면요...
                   </p>
                 </div>
               </div>
