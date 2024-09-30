adb6b9a7f2cf859b339dfddf6d58e5b109e00010
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-09-30 18:27:41 +0900

    [ 💬 | #25 | updateText ] 자기소개 멘트 수정

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,28 +10,31 @@ export function HomePage() {
           <Profile />
           <div className="flex flex-col mt-20 sm:mt-0 sm:w-2/3 sm:ml-auto">
             <p className=" text-4xl text-red_01 font-bold mb-12">
-              안녕하세요. UI디자이너 박우석입니다.
+              안녕하세요. 디자이너 박우석입니다.
             </p>
             <p className=" text-xl font-bold mb-4">
               {'{'} jamooooong {'}'}의 한줄 소개
             </p>
             <p className=" text-base text-gray_02 mb-12">
               안녕하세요 저는 디자인과, 프론트엔드에 관심이 많은 광운대학교
-              19학번 아저씨 입니다.
+              19학번 정부융합학부 학생입니다.
             </p>
             <p className=" text-xl font-bold mb-4">저는 이런거를 잘해요.</p>
             <p className=" text-base text-gray_02 mb-12">
-              안녕하세요 저는 프론트엔드에 관심이 많은 광운대학교 19학번 늙은이
-              입니다. 저는 리액트를 공부하고 있는데 너무 힘들어요. 세상이
-              너무나도 빨리 빨리 변해서 너무 힘들어요.
+              저는 교내 프로젝트 혹은 과제에서 디자이너와 프론트엔드를
+              경험하면서, 개발자의 입장에서 생각하는 디자이너 입니다. 포토샵과
+              일러스트레이터를 다룰줄 알고, 오픈마켓 담당자로 쇼핑몰의
+              상세페이지 및 이미지를 제작한 경험이 있습니다.
             </p>
             <p className=" text-xl font-bold mb-4">
               저는 이런걸 배우고 싶어요.
             </p>
             <p className=" text-base text-gray_02">
-              안녕하세요 저는 프론트엔드에 관심이 많은 광운대학교 19학번 늙은이
-              입니다. 저는 리액트를 공부하고 있는데 너무 힘들어요. 세상이
-              너무나도 빨리 빨리 변해서 너무 힘들어요.
+              Goorm Univ가 저의 첫 해커톤입니다. 프론트엔드 개발자를 지향하면서
+              프로토타입의 중요성을 느꼈고, 디자이너로 성장하고자 Goorm Univ를
+              참여하게되었습니다. 디자이너로서의 스킬과, 기획 및 개발 팀원들간의
+              프로토타입핑을 통한 원활한 소통을 위해 배우고 싶고, 노력하고
+              있습니다.
             </p>
           </div>
         </div>
